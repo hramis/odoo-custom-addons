ceb94e9bc203b3b9a1c642e2cf52d8b2a10dfe78
hramis <123983744+hramis@users.noreply.github.com>
2023-03-13 22:59:05 +0100

    Add files via upload

---
+++ sge1/__manifest__.py
@@ -0,0 +1,13 @@
+{
+    'name': "Lista de Tareas",
+    'version': '0.1',
+    'depends': ['base'],
+    'author': "Hugo Torres",
+    'category': 'Productivity',
+
+    'description': """Sencilla lista de tareas tal tal tal """,
+    # data files always loaded at installation
+    'data': [
+        'views/views.xml'
+    ]
+}
